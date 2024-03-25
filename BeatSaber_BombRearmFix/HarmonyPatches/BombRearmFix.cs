af1a6e1543e8f3c3672847ff7df0417328951ec9
Loloppe <LaurentBlaisCote@outlook.com>
2024-03-25 17:58:00 -0400

    Add project files.

---
+++ BeatSaber_BombRearmFix/HarmonyPatches/BombRearmFix.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+
+namespace BeatSaber_BombRearmFix.HarmonyPatches
+{
+    [HarmonyPatch(typeof(BasicBeatmapObjectManager), nameof(BasicBeatmapObjectManager.DespawnInternal), typeof(NoteController))]
+    internal class DespawnInternalPatch
+    {
+        static bool Prefix(NoteController noteController, ref BasicBeatmapObjectManager __instance)
+        {
+            // Temp fix for the bomb re-arming bug that happen when _memoryPool.Despawn happen. No idea how to actually fix it and doing it this way seems to have no impact whatsoever on the performance so whatever.
+            switch (noteController.noteData.gameplayType)
+            {
+                case NoteData.GameplayType.Normal:
+                    __instance._basicGameNotePoolContainer.Despawn((GameNoteController)noteController);
+                    return false;
+                case NoteData.GameplayType.Bomb:
+                    //__instance._bombNotePoolContainer.Despawn((BombNoteController)noteController);
+                    return false;
+                case NoteData.GameplayType.BurstSliderHead:
+                    __instance._burstSliderHeadGameNotePoolContainer.Despawn((GameNoteController)noteController);
+                    return false;
+                case NoteData.GameplayType.BurstSliderElement:
+                    __instance._burstSliderGameNotePoolContainer.Despawn((BurstSliderGameNoteController)noteController);
+                    return false;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ BeatSaber_BombRearmFix/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BeatSaber_BombRearmFix
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        internal static Harmony Harmony;
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Harmony = new Harmony("Loloppe.BeatSaber.BombRearmFix");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            Harmony.UnpatchSelf();
+        }
+
+    }
+}
