b496571018b9c8ec5f8b567c7d8dc2f44ab165e5
Loloppe <LaurentBlaisCote@outlook.com>
2024-06-16 01:30:54 -0400

    Real fix

--- BeatSaber_BombRearmFix/HarmonyPatches/BombRearmFix.cs
+++ BeatSaber_BombRearmFix/HarmonyPatches/BombRearmFix.cs
@@ -1,25 +1,13 @@
 ﻿using HarmonyLib;
-using UnityEngine;
 
 namespace BeatSaber_BombRearmFix.HarmonyPatches
 {
-    [HarmonyPatch(typeof(BasicBeatmapObjectManager), nameof(BasicBeatmapObjectManager.DespawnInternal), typeof(NoteController))]
-    internal class DespawnInternalPatch
+    [HarmonyPatch(typeof(NoteJump), nameof(NoteJump.Init))]
+    internal class HalfJumpMarkReportedPatch
     {
-        static bool Prefix(NoteController noteController)
+        static void Postfix(ref bool ____halfJumpMarkReported)
         {
-            if (Config.Instance.Enabled && noteController.noteData.gameplayType == NoteData.GameplayType.Bomb && !Plugin.InReplay)
-            {
-                // Hide the bomb model and attempt to destroy it from memory.
-                var bomb = (BombNoteController)noteController;
-                bomb._wrapperGO.SetActive(false);
-                GameObject.Destroy(bomb._wrapperGO);
-                GameObject.Destroy(bomb);
-                return false;
-            }
-
-            // Default behavior if the plugin is disabled or if it's not a bomb.
-            return true;
+            ____halfJumpMarkReported = false;
         }
     }
 }
